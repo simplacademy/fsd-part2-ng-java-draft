f094fc51055959487214f6a54e35889a64bca6db
Chandrasekaran Janardhanan <jjchandru@yahoo.com>
2022-02-23 21:19:19 +0530

    inital commit

---
+++ js/script.js
@@ -0,0 +1,428 @@
+let mediaContent = [
+    {
+     "id": 1,
+     "topic": "Patisserie - Create Menu Page",
+     "subTopic": "Create menu.html",
+     "media": "https:\/\/www.youtube.com\/embed\/q8ky03I1Nco"
+    },
+    {
+     "id": 2,
+     "topic": "Patisserie - Create Menu Page",
+     "subTopic": "Remove row div from menu.html",
+     "media": "https:\/\/www.youtube.com\/embed\/Fu0_60zflCY"
+    },
+    {
+     "id": 3,
+     "topic": "Patisserie - Create Menu Page",
+     "subTopic": "Update Page Heading",
+     "media": "https:\/\/www.youtube.com\/embed\/oPGR86X_vUU"
+    },
+    {
+     "id": 4,
+     "topic": "Patisserie - Create Menu Page",
+     "subTopic": "Creating link between Menu and Our Story",
+     "media": "https:\/\/www.youtube.com\/embed\/xdzwgV3e4vc"
+    },
+    {
+     "id": 5,
+     "topic": "HTML - Learn Anchor Tag",
+     "subTopic": "Create two web pages",
+     "media": "https:\/\/www.youtube.com\/embed\/AbXw9Z4VlzM"
+    },
+    {
+     "id": 6,
+     "topic": "HTML - Learn Anchor Tag",
+     "subTopic": "Include anchor tag for the first page",
+     "media": "https:\/\/www.youtube.com\/embed\/WpfMA88QsvA"
+    },
+    {
+     "id": 7,
+     "topic": "HTML - Learn Anchor Tag",
+     "subTopic": "Include anchor tag for the second page",
+     "media": "https:\/\/www.youtube.com\/embed\/-6OSCkwBBHs"
+    },
+    {
+     "id": 8,
+     "topic": "HTML - Learn Anchor Tag",
+     "subTopic": "Verify the working of anchor tag",
+     "media": "https:\/\/www.youtube.com\/embed\/ibTB2ebbN5c"
+    },
+    {
+     "id": 9,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "How links between Our Story and Menu should work?",
+     "media": "https:\/\/www.youtube.com\/embed\/pCzddolnEKs"
+    },
+    {
+     "id": 10,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Clean up the Visual Studio Code editor files",
+     "media": "https:\/\/www.youtube.com\/embed\/MmWBagEKbFI"
+    },
+    {
+     "id": 11,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Change div to nav in Our Story",
+     "media": "https:\/\/www.youtube.com\/embed\/v6cT9k7vY3w"
+    },
+    {
+     "id": 12,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Check link in Our Story",
+     "media": "https:\/\/www.youtube.com\/embed\/BGHa1grXJpg"
+    },
+    {
+     "id": 13,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Active Navigation Link",
+     "media": "https:\/\/www.youtube.com\/embed\/yXwK-1pOIqQ"
+    },
+    {
+     "id": 14,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Implement Active Navigation Link",
+     "media": "https:\/\/www.youtube.com\/embed\/WJnrl4T0h9c"
+    },
+    {
+     "id": 15,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Customize style of Active Navigation Link",
+     "media": "https:\/\/www.youtube.com\/embed\/J_4SwOUwvQ8"
+    },
+    {
+     "id": 16,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Implement Home link in Our Story",
+     "media": "https:\/\/www.youtube.com\/embed\/M2dddUGHlY8"
+    },
+    {
+     "id": 17,
+     "topic": "Patisserie - Link Our Story and Menu",
+     "subTopic": "Implement navigation links in Home",
+     "media": "https:\/\/www.youtube.com\/embed\/fyBfSvrhtnc"
+    },
+    {
+     "id": 18,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "View Navigation Links in Mobile",
+     "media": "https:\/\/www.youtube.com\/embed\/c4rTPVgMrPA"
+    },
+    {
+     "id": 19,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Introduction to Bootstrap Collapse component",
+     "media": "https:\/\/www.youtube.com\/embed\/Oab8xlbKOEw"
+    },
+    {
+     "id": 20,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Collapse Example",
+     "media": "https:\/\/www.youtube.com\/embed\/ysY09zoLfog"
+    },
+    {
+     "id": 21,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Create HTML for collapse example",
+     "media": "https:\/\/www.youtube.com\/embed\/nckQUcQsXZc"
+    },
+    {
+     "id": 22,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include basic HTML tags with Bootstrap reference",
+     "media": "https:\/\/www.youtube.com\/embed\/3ljDgKO0LBE"
+    },
+    {
+     "id": 23,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include Button",
+     "media": "https:\/\/www.youtube.com\/embed\/S0UWt3-eqqM"
+    },
+    {
+     "id": 24,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Apply styles to Button",
+     "media": "https:\/\/www.youtube.com\/embed\/j0dWro77DU4"
+    },
+    {
+     "id": 25,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include Menu Items",
+     "media": "https:\/\/www.youtube.com\/embed\/5dzhgvQ-lRE"
+    },
+    {
+     "id": 26,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include content after Menu",
+     "media": "https:\/\/www.youtube.com\/embed\/fbh9CoTBoJQ"
+    },
+    {
+     "id": 27,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Define Button as collapse trigger",
+     "media": "https:\/\/www.youtube.com\/embed\/Orn7NYUs6L0"
+    },
+    {
+     "id": 28,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include collapse div",
+     "media": "https:\/\/www.youtube.com\/embed\/o5ORkF7NtpA"
+    },
+    {
+     "id": 29,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Hide menu using collapse class",
+     "media": "https:\/\/www.youtube.com\/embed\/QiHFhi_BNts"
+    },
+    {
+     "id": 30,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include identifier for collapse div",
+     "media": "https:\/\/www.youtube.com\/embed\/ZLy9zh70EyM"
+    },
+    {
+     "id": 31,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include target component reference in Button",
+     "media": "https:\/\/www.youtube.com\/embed\/klnVXGhaHjg"
+    },
+    {
+     "id": 32,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "ARIA Explained",
+     "media": "https:\/\/www.youtube.com\/embed\/klnVXGhaHjg"
+    },
+    {
+     "id": 33,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Button tag code alignment",
+     "media": "https:\/\/www.youtube.com\/embed\/itBj04xeqm8"
+    },
+    {
+     "id": 34,
+     "topic": "Bootstrap - Learn Collapse Component",
+     "subTopic": "Include ARIA attributes in Button",
+     "media": "https:\/\/www.youtube.com\/embed\/0nOQqFQ7WBU"
+    },
+    {
+     "id": 35,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "How Responsive Navbar should work?",
+     "media": "https:\/\/www.youtube.com\/embed\/l3zpJI8G7ps"
+    },
+    {
+     "id": 36,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Include hamburger menu",
+     "media": "https:\/\/www.youtube.com\/embed\/2MiCoBxkEtU"
+    },
+    {
+     "id": 37,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Enclose hamburger menu with Button tag",
+     "media": "https:\/\/www.youtube.com\/embed\/M8wnGjtjnSI"
+    },
+    {
+     "id": 38,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Apply styles for Button",
+     "media": "https:\/\/www.youtube.com\/embed\/S58OiLjXc3o"
+    },
+    {
+     "id": 39,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Include collapse content div",
+     "media": "https:\/\/www.youtube.com\/embed\/olWVEbDQ2Wc"
+    },
+    {
+     "id": 40,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Include all collapse attributes",
+     "media": "https:\/\/www.youtube.com\/embed\/uV8STkwtbjs"
+    },
+    {
+     "id": 41,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Make collapse content responsive using navbar-collapse",
+     "media": "https:\/\/www.youtube.com\/embed\/FltntsxQEXA"
+    },
+    {
+     "id": 42,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Apply spacing around navigation links",
+     "media": "https:\/\/www.youtube.com\/embed\/CBWMltjkSZs"
+    },
+    {
+     "id": 43,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Change container to fluid",
+     "media": "https:\/\/www.youtube.com\/embed\/_D_UiJ-Tt_o"
+    },
+    {
+     "id": 44,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Include navigation changes in Home page",
+     "media": "https:\/\/www.youtube.com\/embed\/5t0GG2Xsw64"
+    },
+    {
+     "id": 45,
+     "topic": "Patisserie - Responsive Navbar",
+     "subTopic": "Regression Testing",
+     "media": "https:\/\/www.youtube.com\/embed\/UmrCO5dfLuw"
+    },
+    {
+     "id": 46,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include row div",
+     "media": "https:\/\/www.youtube.com\/embed\/8-gpAIogBp4"
+    },
+    {
+     "id": 47,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include column div",
+     "media": "https:\/\/www.youtube.com\/embed\/APiqAgI6bDc"
+    },
+    {
+     "id": 48,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Introduction to Bootstrap Card Component",
+     "media": "https:\/\/www.youtube.com\/embed\/-nYDHQUtBek"
+    },
+    {
+     "id": 49,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include card div",
+     "media": "https:\/\/www.youtube.com\/embed\/3r7CDoCcI7o"
+    },
+    {
+     "id": 50,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Organize resource folder",
+     "media": "https:\/\/www.youtube.com\/embed\/TyWDMWmFTtY"
+    },
+    {
+     "id": 51,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Download and Extract resources for Part 2",
+     "media": "https:\/\/www.youtube.com\/embed\/5-Ui8hN5ot4"
+    },
+    {
+     "id": 52,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Copy menu item image",
+     "media": "https:\/\/www.youtube.com\/embed\/xu8xJ6fygM4"
+    },
+    {
+     "id": 53,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Free Stock Images",
+     "media": "https:\/\/www.youtube.com\/embed\/oFN9rpHQPaU"
+    },
+    {
+     "id": 54,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include menu item image",
+     "media": "https:\/\/www.youtube.com\/embed\/AH6fUZa2d7w"
+    },
+    {
+     "id": 55,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Change container to fluid",
+     "media": "https:\/\/www.youtube.com\/embed\/uc8UXTGIxes"
+    },
+    {
+     "id": 56,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include menu item name",
+     "media": "https:\/\/www.youtube.com\/embed\/jtBtHNy27Ds"
+    },
+    {
+     "id": 57,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Style menu item name",
+     "media": "https:\/\/www.youtube.com\/embed\/Wc0RdKDtKas"
+    },
+    {
+     "id": 58,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include menu item description",
+     "media": "https:\/\/www.youtube.com\/embed\/HDfRMTO9p0c"
+    },
+    {
+     "id": 59,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Style menu item description",
+     "media": "https:\/\/www.youtube.com\/embed\/KPOq3lP-VFg"
+    },
+    {
+     "id": 60,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Div tag layout for price and add button",
+     "media": "https:\/\/www.youtube.com\/embed\/8808fwHl8jE"
+    },
+    {
+     "id": 61,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include div tags for price and add button",
+     "media": "https:\/\/www.youtube.com\/embed\/C0KlqWbYGwI"
+    },
+    {
+     "id": 62,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include price",
+     "media": "https:\/\/www.youtube.com\/embed\/kHgldOcCdY8"
+    },
+    {
+     "id": 63,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Style price",
+     "media": "https:\/\/www.youtube.com\/embed\/OGfC1tmf838"
+    },
+    {
+     "id": 64,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Introduction to Bootstrap Button Component",
+     "media": "https:\/\/www.youtube.com\/embed\/Jmm5hwEqnAM"
+    },
+    {
+     "id": 65,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Include Add Button",
+     "media": "https:\/\/www.youtube.com\/embed\/JfqeVBe7c1o"
+    },
+    {
+     "id": 66,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Style Add Button",
+     "media": "https:\/\/www.youtube.com\/embed\/YK0f6ZmKEOM"
+    },
+    {
+     "id": 67,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Style Add Button on Hover",
+     "media": "https:\/\/www.youtube.com\/embed\/6VvfvfCj88A"
+    },
+    {
+     "id": 68,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Right Align Add Button",
+     "media": "https:\/\/www.youtube.com\/embed\/DJEPi-yqtxI"
+    },
+    {
+     "id": 69,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Adding next menu item",
+     "media": "https:\/\/www.youtube.com\/embed\/aM1iSdpTbfA"
+    },
+    {
+     "id": 70,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Challenges in managing Patisserie menu page",
+     "media": "https:\/\/www.youtube.com\/embed\/2rimEkw0lc8"
+    },
+    {
+     "id": 71,
+     "topic": "Patisserie - Display one menu item",
+     "subTopic": "Limitations of HTML, CS and Bootstrap",
+     "media": "https:\/\/www.youtube.com\/embed\/ErvFlEYNWmQ"
+    }
+   ];
